e3176a567f995bbc0b2e3461d8d8e182269e36d0
Saunved <saunved@gmail.com>
2021-07-25 13:40:09 +0530

    feat: added a click handler to connect wallet

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
-const Button = ({classes, text}: {classes: string, text: string}) => {
+const Button = ({classes, text, onClick}: {classes: string, text: string, onClick: React.MouseEventHandler<HTMLButtonElement>}) => {
     return (
-        <button className={classes}>
+        <button onClick={onClick} className={classes}>
             {text}
         </button>
     )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,16 +6,20 @@ const T_CONNECT_WALLET = 'Connect to a wallet'
 
 const Navbar = () => {
 
+    const walletConnectionHandler = () => {
+        console.log('Ask to connect wallet')
+    }
+
     return (
         <nav>
             <div className="flex justify-between py-4 border-b border-gray-800 pl-4 pr-2">
                 <div className="grid grid-flow-col gap-8 items-center">
                     <img src="http://via.placeholder.com/42x42" className="h-10 w-10" alt="" />
-                    <Link to="/swap" className="text-gray-300 hover:text-gray-50">{T_SWAP}</Link>
-                    <Link to="/pool" className="text-gray-300 hover:text-gray-50">{T_POOL}</Link>
+                    <Link to="/swap" className="hm-link">{T_SWAP}</Link>
+                    <Link to="/pool" className="hm-link">{T_POOL}</Link>
                 </div>
                 <div>
-                    <Button text={T_CONNECT_WALLET} classes="bg-gray-800 text-gray-200 px-4 py-2 rounded hover:bg-gray-700"></Button>
+                    <Button text={T_CONNECT_WALLET} classes="hm-btn hm-btn-light" onClick={walletConnectionHandler}></Button>
                 </div>
             </div>
         </nav>
