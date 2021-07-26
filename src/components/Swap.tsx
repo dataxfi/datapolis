e420ecfe4045e7674ac4488f9c1afd85a233f56e
Saunved <saunved@gmail.com>
2021-07-27 02:00:01 +0530

    multiple fixes: * responsiveness for input elements * disabled scrolling on the numeric input to prevent negative number entries

--- src/components/Swap.tsx
+++ src/components/Swap.tsx
@@ -9,7 +9,7 @@ const text = {
 
 const Swap = () => {
     return (
-        <div className="flex mt-32 w-full items-center">
+        <div className="flex mt-16 md:mt-32 w-full items-center">
             <div className="max-w-3xl mx-auto bg-black w-full rounded-lg p-4">
                 <p className="text-xl">Swap</p>
                 <SwapInput title={text.T_SWAP_FROM} />
--- src/components/SwapInput.tsx
+++ src/components/SwapInput.tsx
@@ -1,7 +1,8 @@
 const SwapInput = ({title}: {title: string}) => {
+
     return (
         <div className="mt-4 bg-gray-800 p-4 rounded-lg">
-        <div className="grid grid-cols-5">
+        <div className="md:grid md:grid-cols-5">
             <div className="col-span-2 grid grid-flow-col gap-4 justify-start items-center">
                 <img src="http://via.placeholder.com/70x70" className="w-16 h-16 rounded-md" alt="" />
                 <div>
@@ -9,9 +10,9 @@ const SwapInput = ({title}: {title: string}) => {
                     <p className="text-2xl text-gray-200 font-bold">ETH</p>
                 </div>
             </div>
-            <div className="col-span-3">
-                {/* https://stackoverflow.com/a/58097342/6513036 */}
-                <input onKeyDown={(evt) => ["e", "E", "+", "-"].includes(evt.key) && evt.preventDefault()} type="number" className="h-full w-full rounded-lg bg-black text-4xl px-2 outline-none focus:placeholder-gray-200 placeholder-gray-500" placeholder="0.0" />
+            <div className="col-span-3 mt-3 md:mt-0">
+                {/* https://stackoverflow.com/a/58097342/6513036 and https://stackoverflow.com/a/62275278/6513036 */}
+                <input onWheel={ event => event.currentTarget.blur() } onKeyDown={(evt) => ["e", "E", "+", "-"].includes(evt.key) && evt.preventDefault()} type="number" className="h-full w-full rounded-lg bg-black text-4xl px-2 outline-none focus:placeholder-gray-200 placeholder-gray-500" placeholder="0.0" />
             </div>
         </div>
     </div>
