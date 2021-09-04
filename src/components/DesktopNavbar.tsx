427be3d0e8bcf2abc65fa1eeedd1b20f2be15bed
0xhashmesh <hashmesh.labs@gmail.com>
2021-09-04 18:53:24 +0200

    cosmetic additions

--- src/components/DesktopNavbar.tsx
+++ src/components/DesktopNavbar.tsx
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react'
 import { Link } from 'react-router-dom'
+import {ReactComponent as Logo} from '../assets/logo.svg';
 import Button from './Button'
 import {GlobalContext} from '../context/GlobalState'
 
@@ -8,11 +9,11 @@ const DesktopNavbar = ({links, text, wallet}: {links: Array<any>, text: Record<a
     const {handleConnect, buttonText } = useContext(GlobalContext)
 
     return (
-             <div className="md:flex justify-between items-center py-4 border-b border-gray-800 pl-4 pr-2 hidden">
+             <div className="md:flex justify-between items-center py-4 border-gray-800 pl-4 pr-2 hidden">
                 <div className="grid grid-flow-col gap-8 items-center">
-                    <img src="http://via.placeholder.com/42x42" className="h-10 w-10" alt="" />
+                    <Logo className="logo"/>
                     {links.map((link, idx) => {
-                        return <Link key={idx} to={link.link} className="hm-link hidden md:block">{link.name}</Link>    
+                        return <Link key={idx} to={link.link} className="hm-link hidden md:block product">{link.name}</Link>    
                     })}
                 </div>
                 <div className="hidden md:block">
