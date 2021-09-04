cd39868e7e5fea5f043050294079423b94975448
Saunved <saunved@gmail.com>
2021-09-04 17:58:32 +0530

    feat: css theme changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,68 +1,75 @@
-  // tailwind.config.js
-const colors = require('tailwindcss/colors')
+// tailwind.config.js
+const colors = require("tailwindcss/colors");
 
-  module.exports = {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-     darkMode: false, // or 'media' or 'class'
-     theme: {
-       fontFamily: {
-         'sans': ['DM Sans', 'Helvetica', 'Arial', 'sans-serif']
-       },
-       extend: {
-         colors: {
-          'primary': colors.trueGray,
-          'secondary': colors.blue,
-          'tertiary': colors.coolGray,
-          'type': colors.warmGray,
-          'background': colors.black
-         }
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["DM Sans", "Helvetica", "Arial", "sans-serif"],
+    },
+    extend: {
+      //  colors: {
+      //   'primary': colors.trueGray,
+      //   'secondary': colors.blue,
+      //   'tertiary': colors.coolGray,
+      //   'type': colors.warmGray,
+      //   'background': colors.black
+      //  }
 
-        // colors: {
-        //   'primary': colors.emerald,
-        //   'secondary': colors.blue,
-        //   'tertiary': colors.coolGray,
-        //   'type': colors.warmGray,
-        //   'background': colors.black
-        //  }   
+      colors: {
+        primary: colors.coolGray,
+        secondary: colors.indigo,
+        tertiary: colors.sky,
+        type: colors.warmGray,
+        background: colors.black,
+      },
 
-        // colors: {
-        //   'primary': colors.fuchsia,
-        //   'secondary': colors.green,
-        //   'tertiary': colors.coolGray,
-        //   'type': colors.warmGray,
-        //   'background': colors.black
-        //  }
+      // colors: {
+      //   'primary': colors.emerald,
+      //   'secondary': colors.blue,
+      //   'tertiary': colors.coolGray,
+      //   'type': colors.warmGray,
+      //   'background': colors.black
+      //  }
 
-        //  colors: {
-        //   'primary': colors.fuchsia,
-        //   'secondary': colors.green,
-        //   'tertiary': colors.coolGray,
-        //   'type': colors.warmGray,
-        //   'background': colors.purple[900]
-        //  }  
-        
-        //  colors: {
-        //   'primary': colors.fuchsia,
-        //   'secondary': colors.green,
-        //   'tertiary': colors.coolGray,
-        //   'type': {
-        //     50: colors.warmGray[900],
-        //     100: colors.warmGray[900],
-        //     200: colors.warmGray[800],
-        //     300: colors.warmGray[700],
-        //     400: colors.warmGray[600],
-        //     600: colors.warmGray[400],
-        //     700: colors.warmGray[300],
-        //     800: colors.warmGray[200],
-        //     900: colors.warmGray[100],
-        //   },
-        //   'background': colors.purple[100]
-        //  }          
-        
-       },
-     },
-     variants: {
-       extend: {},
-     },
-     plugins: [],
-   }
+      // colors: {
+      //   'primary': colors.fuchsia,
+      //   'secondary': colors.green,
+      //   'tertiary': colors.coolGray,
+      //   'type': colors.warmGray,
+      //   'background': colors.black
+      //  }
+
+      //  colors: {
+      //   'primary': colors.fuchsia,
+      //   'secondary': colors.green,
+      //   'tertiary': colors.coolGray,
+      //   'type': colors.warmGray,
+      //   'background': colors.purple[900]
+      //  }
+
+      //  colors: {
+      //   'primary': colors.fuchsia,
+      //   'secondary': colors.green,
+      //   'tertiary': colors.coolGray,
+      //   'type': {
+      //     50: colors.warmGray[900],
+      //     100: colors.warmGray[900],
+      //     200: colors.warmGray[800],
+      //     300: colors.warmGray[700],
+      //     400: colors.warmGray[600],
+      //     600: colors.warmGray[400],
+      //     700: colors.warmGray[300],
+      //     800: colors.warmGray[200],
+      //     900: colors.warmGray[100],
+      //   },
+      //   'background': colors.purple[100]
+      //  }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
