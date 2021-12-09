fc952ab7f796b175453b4a587f282dd986bdf0f5
Keith Frazier <keithers98@gmail.com>
2021-12-08 17:04:50 -0800

    pending indicator opens txHistory modal onClick

--- src/components/PendingTxsIndicator.tsx
+++ src/components/PendingTxsIndicator.tsx
@@ -4,11 +4,11 @@ import { GlobalContext } from "../context/GlobalState"
 
 export default function PendingTxsIndicator (){
 
-    const {accountId, pendingTxs} = useContext(GlobalContext)
+    const {accountId, pendingTxs, setShowPendingTxsModal } = useContext(GlobalContext)
 
 
     return  accountId && pendingTxs.length > 0 ? (
-        <div className="block capitalize border border-type-500 text-type-200 rounded-md px-4 py-1 hm-box ">
+        <div onClick={() => setShowPendingTxsModal(true)} className="block capitalize border border-type-500 text-type-200 rounded-md px-4 py-1 hm-box transition-all ease-in-out transform hover:bg-primary-400 hover:bg-opacity-20">
           <div className="pr-3 flex flex-row">
             {`${pendingTxs.length} Pending`}{" "}
             <div className="pt-1 pl-1">
