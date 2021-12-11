03ff8388090e14b0a632f85c61112e3691a29c6e
Keith Frazier <keithers98@gmail.com>
2021-12-10 16:49:28 -0800

    Changed X to a Book icon, added element titles
    Hovering an item in the footer will show a description of where it goes
    
    issue #39

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,7 @@ import {
   faMediumM,
   faGithub,
 } from "@fortawesome/free-brands-svg-icons";
+import { FaBook } from "react-icons/fa";
 import { ReactComponent as DataXIconGrey150 } from "../assets/DataXIconGrey150.svg";
 export default function Footer() {
   return (
@@ -12,65 +13,60 @@ export default function Footer() {
       <div className="flex flex-col text-center">
         <ul className="inline-flex text-2xl justify-center">
           <li className="list-inline-item mr-2">
-            <a
-              href="https://twitter.com/dataX_fi"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faTwitter}
-                className="transition duration-250 ease-in-out transform hover:-translate-y-1 hover:scale-110"
-              />
-            </a>
+            <div title="Twitter">
+              <a
+                href="https://twitter.com/dataX_fi"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+            </div>
           </li>
           <li className="list-inline-item mr-2">
-            <a
-              href="https://discord.com/invite/b974xHrUGV"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faDiscord}
-                className="transition duration-250 ease-in-out transform hover:-translate-y-1 hover:scale-110"
-              />
-            </a>
+            <div title="Discord">
+              <a
+                href="https://discord.com/invite/b974xHrUGV"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faDiscord} />
+              </a>
+            </div>
           </li>
           <li className="list-inline-item mr-2">
-            <a
-              href="https://medium.com/datax-finance"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faMediumM}
-                className="transition duration-250 ease-in-out transform hover:-translate-y-1 hover:scale-110"
-              />
-            </a>
+            <div title="Medium">
+              <a
+                href="https://medium.com/datax-finance"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faMediumM} />
+              </a>
+            </div>
           </li>
           <li className="list-inline-item mr-2 self-center">
-            <a
-              href="https://datax.gitbook.io/datax/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <DataXIconGrey150
-                className="self-center pb-0.5 transition duration-250 ease-in-out transform hover:-translate-y-1 hover:scale-110"
-                style={{ height: "28px" }}
-              />
-            </a>
+            <div title="DataX Docs">
+              <a
+                href="https://datax.gitbook.io/datax/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaBook size="26px" />
+              </a>
+            </div>
           </li>
           <li className="list-inline-item">
-            <a
-              href="https://github.com/dataxfi/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              {" "}
-              <FontAwesomeIcon
-                icon={faGithub}
-                className="transition duration-250 ease-in-out transform hover:-translate-y-1 hover:scale-110"
-              />
-            </a>
+            <div title="GitHub">
+              <a
+                href="https://github.com/dataxfi/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {" "}
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+            </div>
           </li>
         </ul>
         <p>Copyright © DataX 2021</p>
