748e12c60489b555c8f101bfd456ba6f300c35da
Keith Frazier <keithers98@gmail.com>
2022-04-04 10:26:30 -0700

    renamed dataset preview test file

--- src/context/GlobalState.tsx
+++ src/context/GlobalState.tsx
@@ -1,6 +1,5 @@
 import Web3 from "web3";
-import Watcher from "@dataxfi/datax.js/dist/Watcher";
-import { Ocean, Config } from "@dataxfi/datax.js";
+import { Ocean, Config, Watcher } from "@dataxfi/datax.js";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import { createContext, PropsWithChildren, useEffect, useRef, useState } from "react";
