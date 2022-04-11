bbe701d5002af919b143bed50ad2799b21c8e3a6
Keith Frazier <keithers98@gmail.com>
2022-04-11 10:59:27 -0700

    progress bar on snackbar

--- src/components/Snackbar.tsx
+++ src/components/Snackbar.tsx
@@ -4,16 +4,15 @@ import { ISnackbarItem, ITxDetails } from "../utils/types";
 import { BsX, BsXCircle } from "react-icons/bs";
 import { IoCheckboxOutline } from "react-icons/io5";
 import { getTxUrl } from "../hooks/useTxHistory";
-import BigNumber from "bignumber.js";
-
+import BigNumber from 'bignumber.js'
 export default function Snackbar() {
   const { snackbarItem, setSnackbarItem, ocean, accountId } = useContext(GlobalContext);
   const [currentNot, setCurrentNot] = useState<ISnackbarItem>();
-  const [opacity, setOpacity] = useState<string>("0");
+  const [opacity, setOpacity] = useState<string>("100");
   const [txDetails, setTxDetails] = useState<ITxDetails>();
   const [url, setUrl] = useState<string>();
   const [cleanup, setCleanup] = useState<boolean>(true);
-
+  const [progress, setProgress] = useState<string>("100%");
   useEffect(() => {
     if (snackbarItem && !currentNot) {
       console.log(snackbarItem);
@@ -24,8 +23,7 @@ export default function Snackbar() {
   }, [snackbarItem, currentNot]);
 
   useEffect(() => {
-    if (ocean && accountId && currentNot?.newTx)
-      setUrl(getTxUrl({ ocean, accountId, txHash: currentNot.newTx.txReceipt?.transactionHash }));
+    if (ocean && accountId && currentNot?.newTx) setUrl(getTxUrl({ ocean, accountId, txHash: currentNot.newTx.txReceipt?.transactionHash }));
     setTxDetails(currentNot?.newTx);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ocean, accountId, currentNot]);
@@ -56,6 +54,10 @@ export default function Snackbar() {
       setOpacity("100");
     }, 500);
 
+    setTimeout(()=>{
+      setProgress("0%")
+    }, 500)
+
     setTimeout(() => {
       setOpacity("0");
     }, 5500);
@@ -68,33 +70,24 @@ export default function Snackbar() {
 
   if (!currentNot) return <></>;
   return (
-    <div className={`max-w-xs fixed right-2 top-16 md:right-8 w-full`}>
-      <div
-        className={`max-w-xs w-full mx-auto bg-black bg-opacity-90 rounded-lg p-1 lg:p-4 transition-opacity ease-in-out opacity-${opacity} duration-500`}
-      >
+    <div className={`max-w-xs fixed right-2 top-18 md:right-8 w-full`}>
+      <div className={`relative top-0 left-0 max-w-xs w-full mx-auto bg-black bg-opacity-90 rounded-lg p-1 lg:p-4 transition-opacity ease-in-out opacity-${opacity} duration-500`}>
         <div className="flex justify-between items-center">
           <div className="grid grid-flow-col gap-4 items-center">
-            {txDetails && currentNot.type === "tx" ? (
+            {(txDetails && currentNot?.type === "tx")? (
               <>
                 <IoCheckboxOutline size="24" className="text-city-blue" />
                 <div>
-                  {/* <p className="text-gray-100 text-sm">{lastTx.txType}</p> */}
                   <p>
                     {txDetails.txType === "stake" && txDetails.token2.info
-                      ? `Stake ${new BigNumber(txDetails.token1.value).dp(5).toString()} OCEAN in ${
-                          txDetails.token2.info.symbol
-                        }/OCEAN pool`
+                      ? `Stake ${new BigNumber(txDetails.token1.value).dp(5).toString()} OCEAN in ${txDetails.token2.info.symbol}/OCEAN pool`
                       : txDetails.txType === "unstake" && txDetails.shares && txDetails.token2.info
-                      ? `Unstake ${new BigNumber(txDetails.shares).dp(5).toString()} OCEAN from ${
-                          txDetails.token2.info.symbol
-                        }/OCEAN pool`
+                      ? `Unstake ${new BigNumber(txDetails.shares).dp(5).toString()} OCEAN from ${txDetails.token2.info.symbol}/OCEAN pool`
                       : txDetails.txType === "approve"
                       ? `Unlock ${txDetails.token1.info?.symbol}`
-                      : `Trade ${new BigNumber(txDetails.token1.value).dp(5).toString()} ${
-                          txDetails.token1.info?.symbol
-                        } for ${new BigNumber(txDetails.token2.value).dp(5).toString()} ${
-                          txDetails.token2.info?.symbol
-                        }`}
+                      : `Trade ${new BigNumber(txDetails.token1.value).dp(5).toString()} ${txDetails.token1.info?.symbol} for ${new BigNumber(txDetails.token2.value)
+                          .dp(5)
+                          .toString()} ${txDetails.token2.info?.symbol}`}
                   </p>
                   <p className="text-gray-300 text-sm">
                     <a target="_blank" rel="noreferrer" href={url} className="hover:text-city-blue">
@@ -103,7 +96,7 @@ export default function Snackbar() {
                   </p>
                 </div>
               </>
-            ) : currentNot.type === "error" ? (
+            ) : currentNot?.type === "error" ? (
               <>
                 <BsXCircle className="text-red-500 mr-4" />
                 <p>{errorMessage(currentNot)}</p>
@@ -122,6 +115,9 @@ export default function Snackbar() {
               }}
             />
           </div>
+        </div>
+        <div className="bottom-0 left-0 w-full bg-gray-900 h-2 absolute rounded-b-full overflow-clip">
+          <div className="h-full bg-stake-blue bg-opacity-60 transition-all transform duration-[5400ms] ease-linear rounded-bl-full rounded-r-full" style={{ width: progress }} />
         </div>
       </div>
     </div>
